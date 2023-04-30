6239961ffbe4e3dc90fd2b72599afee2035b08de
karaelf33 <bestasyusuf8@gmail.com>
2023-04-30 21:00:51 +0200

    wild card deleted

--- src/main/java/com/example/documentindex/controller/DocumentController.java
+++ src/main/java/com/example/documentindex/controller/DocumentController.java
@@ -7,7 +7,11 @@ import com.example.documentindex.dto.response.DocumentResponse;
 import com.example.documentindex.dto.response.SearchResponse;
 import com.example.documentindex.service.DocumentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
