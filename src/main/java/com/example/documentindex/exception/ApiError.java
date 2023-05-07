f0dbf8573c143edf69509d2547867fc8772e9b14
karaelf33 <bestasyusuf8@gmail.com>
2023-05-07 18:00:44 +0200

    Global exception handler added

---
+++ src/main/java/com/example/documentindex/exception/ApiError.java
@@ -0,0 +1,21 @@
+package com.example.documentindex.exception;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Setter
+public class ApiError {
+
+    private HttpStatus status;
+    private String message;
+    private Throwable cause;
+
+    public ApiError(HttpStatus status, String message, Throwable cause) {
+        this.status = status;
+        this.message = message;
+        this.cause = cause;
+    }
+
+}
